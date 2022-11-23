c269906fe66a9ee2c3ff9e9542b82446b4a6fa64
Mornieur <mariafernanda-coro@hotmail.com>
2022-11-23 14:44:49 -0300

    feat: changing file name

--- src/components/CardEmotion/index.tsx
+++ src/components/CardEmotion/index.tsx
@@ -1,4 +1,4 @@
-import  blogList from '../../data/blogList.json';
+import  dataCharacter from '../../data/dataCharacter.json';
 import { motion, Variants } from 'framer-motion';
 import { FC } from 'react';
 import * as S from './styles';
@@ -6,16 +6,12 @@ import {PhotoComp} from "../PhotoComp"
 import denji from "../../../public/assets/denji.jpg"
 import fakeImage from "../../../public/assets/fakeImage.png"
 import Image from 'next/image';
-import { CardAnimation } from '../CardAnimation';
-
-
-
-
+import { CardAnimation } from './CardAnimation';
 
 export const CardEmotion: FC = () => {
   return (
     <S.Container>
-      {blogList.map((item, index) => (
+      {dataCharacter.map((item, index) => (
         <div className="card-wrapper" key={index}>
           <CardAnimation image={item.image} h2={item.h2} p={item.p} />
         </div>
