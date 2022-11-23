4cfd782293f44485bdf920c1b7cda2394eb5a124
Mornieur <mariafernanda-coro@hotmail.com>
2022-11-23 14:46:16 -0300

    feat: adding global styled

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
 import '../global/styles/globals'
 import type { AppProps } from 'next/app'
+import { GlobalStyle } from '../global/styles/globals'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+   return (
+   <>
+  <GlobalStyle />
+  <Component {...pageProps} />
+  </> 
+   )
+  
+ 
 }
