d4ff842b9cc9d61b291aeb27d871408f5b2b9beb
Mornieur <mariafernanda-coro@hotmail.com>
2022-11-23 23:07:50 -0300

    feat: adding gif item in card

--- src/components/CardEmotion/index.tsx
+++ src/components/CardEmotion/index.tsx
@@ -1,11 +1,7 @@
 import  dataCharacter from '../../data/dataCharacter.json';
-import { motion, Variants } from 'framer-motion';
+
 import { FC } from 'react';
 import * as S from './styles';
-import {PhotoComp} from "../PhotoComp"
-import denji from "../../../public/assets/denji.jpg"
-import fakeImage from "../../../public/assets/fakeImage.png"
-import Image from 'next/image';
 import { CardAnimation } from './CardAnimation';
 
 export const CardEmotion: FC = () => {
@@ -13,7 +9,7 @@ export const CardEmotion: FC = () => {
     <S.Container>
       {dataCharacter.map((item, index) => (
         <div className="card-wrapper" key={index}>
-          <CardAnimation image={item.image} h2={item.h2} p={item.p} />
+          <CardAnimation image={item.image} h2={item.h2} p={item.p} gif={item.gif} />
         </div>
       ))}
     </S.Container>
